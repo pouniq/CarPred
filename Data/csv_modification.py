1109d34bd4007490c7da4fe40bab6e89ea629ccb
pouniq <abdollahpora@gmail.com>
2026-07-30 14:37:37 +0330

    drop the duplicates for final.csv

--- Data/csv_modification.py
+++ Data/csv_modification.py
@@ -8,6 +8,12 @@ lil_data = pd.read_csv("CSV/divar_listings.csv")
 # first extract the date of production from the `car` column
 # and for cars that do not have them look for that manually
 df = pd.concat([data1, data2], ignore_index=True)
-# first data loaded to csv
 
+
+cols_to_check = [c for c in df.columns if c != "link"]
+
+df.duplicated(subset=cols_to_check).sum()
+df = df.drop_duplicates(subset=cols_to_check)
+
+# save to csv
 df.to_csv('CSV/final.csv', index=False, encoding='utf-8')
