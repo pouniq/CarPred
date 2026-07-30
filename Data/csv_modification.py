58c11edfbc636a4951e6b89f0856d6310edc3dc4
pouniq <abdollahpora@gmail.com>
2026-07-30 21:30:05 +0330

    change the price & mileage to english for numerical features;

--- Data/csv_modification.py
+++ Data/csv_modification.py
@@ -1,19 +1,34 @@
 import pandas as pd
+import re
 
 
 data1 = pd.read_csv('CSV/output.csv')
 data2 = pd.read_csv('CSV/output1.csv')
+data3 = pd.read_csv('CSV/output2.csv')
 lil_data = pd.read_csv("CSV/divar_listings.csv")
 
 # first extract the date of production from the `car` column
 # and for cars that do not have them look for that manually
-df = pd.concat([data1, data2], ignore_index=True)
+df = pd.concat([data1, data2,data3], ignore_index=True)
 
 
 cols_to_check = [c for c in df.columns if c != "link"]
 
 df.duplicated(subset=cols_to_check).sum()
 df = df.drop_duplicates(subset=cols_to_check)
+
+
+persian_digits = "۰۱۲۳۴۵۶۷۸۹"
+translation = str.maketrans(persian_digits, "0123456789")
+
+def extract_numbers(text):
+    normalized = text.translate(translation)
+    normalized = normalized.replace(",", "").replace("،", "")  # strip both comma types
+    return [int(n) for n in re.findall(r"\d+", normalized)]
+
+
+df["mileage"] = df["mileage"].apply(lambda x: extract_numbers(x)[0] if extract_numbers(x) else None)
+df['date'] = df['date'].apply(lambda x: extract_numbers(x)[0] if extract_numbers(x) else None)
 
 # save to csv
 df.to_csv('CSV/final.csv', index=False, encoding='utf-8')
