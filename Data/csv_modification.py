a8a21707fe598965a7ef5ceda0423f86cddb7240
pouniq <abdollahpora@gmail.com>
2026-07-31 14:37:23 +0330

    made sub colors to the whole color to make the category make sense

--- Data/csv_modification.py
+++ Data/csv_modification.py
@@ -2,14 +2,20 @@ import pandas as pd
 import re
 
 
-data1 = pd.read_csv('CSV/output.csv')
-data2 = pd.read_csv('CSV/output1.csv')
-data3 = pd.read_csv('CSV/output2.csv')
-lil_data = pd.read_csv("CSV/divar_listings.csv")
+# data1 = pd.read_csv('CSV/output.csv')
+# data2 = pd.read_csv('CSV/output1.csv')
+# data3 = pd.read_csv('CSV/output2.csv')
+# lil_data = pd.read_csv("CSV/divar_listings.csv")
+
+
 
 # first extract the date of production from the `car` column
 # and for cars that do not have them look for that manually
-df = pd.concat([data1, data2,data3], ignore_index=True)
+# df = pd.concat([data1, data2,data3], ignore_index=True)
+
+
+df = pd.read_csv("CSV/final.csv")
+
 
 
 cols_to_check = [c for c in df.columns if c != "link"]
@@ -29,6 +35,28 @@ def extract_numbers(text):
 
 df["mileage"] = df["mileage"].apply(lambda x: extract_numbers(x)[0] if extract_numbers(x) else None)
 df['date'] = df['date'].apply(lambda x: extract_numbers(x)[0] if extract_numbers(x) else None)
+
+df_fin['color'].unique()
+
+
+# to make the color feature more in place
+# I made colors that look like each other into one 
+# category
+
+color_rep = {
+    'نوک‌مدادی': 'Gray',
+    'مشکی' : 'Black',
+    'سفید': 'White',
+    'موکا': 'Brown',
+    'قهوه‌ای': 'Brown',
+    'طوسی' : 'Gray',
+   'نقره‌ای' : 'Gray',
+   'خاکستری' : 'Gray',
+   'بژ': 'Brown'
+}
+
+df_fin['color'] = df_fin['color'].replace(color_rep)
+df
 
 # save to csv
 df.to_csv('CSV/final.csv', index=False, encoding='utf-8')
--- EDA/ExDaAn.py
+++ EDA/ExDaAn.py
@@ -6,3 +6,12 @@ import seaborn as sns
 df_link = "../Data/CSV/final.csv"
 
 df = pd.read_csv(df_link)
+df = df.drop(columns=['link','price_toman'])
+
+df.describe()
+
+print(df.value_counts())
+plt.bar(df['date'], df['mileage'])
+plt.show()
+
+
