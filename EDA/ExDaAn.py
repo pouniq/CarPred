820d8dc334e266fb593e128d4ba115a500562178
pouniq <abdollahpora@gmail.com>
2026-07-31 15:09:49 +0330

    some EDA was done here

--- EDA/ExDaAn.py
+++ EDA/ExDaAn.py
@@ -3,15 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+plt.rcParams['axes.labelsize'] = 8     # xlabel/ylabel
+
+
 df_link = "../Data/CSV/final.csv"
 
 df = pd.read_csv(df_link)
 df = df.drop(columns=['link','price_toman'])
 
+
+num_cols = df.select_dtypes(np.number)
+cat_cols = df.select_dtypes(str)
 df.describe()
 
 print(df.value_counts())
-plt.bar(df['date'], df['mileage'])
+
+
+df.groupby('color').value_counts()
+
+num_cols.corr()
+sns.heatmap(num_cols.corr(), cmap = 'coolwarm')
+plt.show()
+
+
+
+plt.scatter(df['date'], df['mileage'])
+plt.xlabel('Date of production')
+plt.ylabel('Mileage of the car')
 plt.show()
 
 
